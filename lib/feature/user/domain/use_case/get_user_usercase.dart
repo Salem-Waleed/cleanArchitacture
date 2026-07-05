45d9fb18452787e84e37a35dc456d1f95c160528
salem waleed <salembokir03@gmail.com>
2026-07-06 00:11:40 +0300

    add user params

--- lib/feature/user/domain/use_case/get_user_usercase.dart
+++ lib/feature/user/domain/use_case/get_user_usercase.dart
@@ -1,13 +1,14 @@
 import 'package:clean_architacture/core/errors/failure.dart';
+import 'package:clean_architacture/core/params/params.dart';
 import 'package:clean_architacture/feature/user/domain/entities/user_entity.dart';
 import 'package:clean_architacture/feature/user/domain/repository/user_repository.dart';
 import 'package:dartz/dartz.dart';
 
 class GetUserUsercase {
-  UserRepository repository;
+  final UserRepository repository;
 
   GetUserUsercase({required this.repository});
-  Future<Either<Failure, UserEntity>> call() {
-    return repository.getUser();
+  Future<Either<Failure, UserEntity>> call({required UserParams params}) {
+    return repository.getUser(params: params);
   }
 }
